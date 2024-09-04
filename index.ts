dbf9e94eac636376b0d4c79bb8ad44ba1693b936
= <=>
2024-09-04 13:51:38 +0200

    ts1

---
+++ index.ts
@@ -0,0 +1,16 @@
+import { Diak, Osztaly } from "./types";
+
+let greet: string = "Hello TS!";
+let isTrue: boolean;
+
+isTrue = false;
+
+//let pdw: HTMLInputElement = document.getElementById("password") as HTMLInputElement;
+
+let Peti: Diak;
+
+Peti = {name: "Péter", age: 17, class: {evfolyam: 10, betujel: 'A'}}
+
+
+console.log(Peti);
+
